b6c2d4d9f33153ddee9d1229cf7cd973f81ad901
barnabew <barnabe.willenbucher@outlook.fr>
2025-10-14 19:50:17 +0200

    Create app_churn.py

---
+++ streamlit/app_churn.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Analyse et Prédiction du Churn Client",
+    page_icon="📉",
+    layout="wide"
+)
+
+st.title("📊 Projet : Prédiction du Churn Client Télécom")
+st.write("Bienvenue dans l’application d’analyse et de prédiction du churn.")
+
+st.sidebar.success("Choisissez une page ci-dessus pour commencer.")

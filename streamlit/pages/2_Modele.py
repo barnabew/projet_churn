6ccc376d49186f18ce2f15a53894f90756794fd5
barnabew <barnabe.willenbucher@outlook.fr>
2025-10-15 12:58:28 +0200

    Update 2_Modele.py

--- streamlit/pages/2_Modele.py
+++ streamlit/pages/2_Modele.py
@@ -1,3 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+
 # --- PAGE CONFIG ---
 st.set_page_config(page_title="Prédiction de Churn", page_icon="📉", layout="centered")
 

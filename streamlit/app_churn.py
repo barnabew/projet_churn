7491afc41ab6145de99ecaf8a398f0de330c3313
barnabew <barnabe.willenbucher@outlook.fr>
2025-10-15 12:51:30 +0200

    Update app_churn.py

--- streamlit/app_churn.py
+++ streamlit/app_churn.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from ml import reg_lineaire
+from data import chargement_nettoyage
+import pickle
+
+with open("modele.pkl", "wb") as f:
+    pickle.dump(reg_lineaire(chargement_nettoyage())), f)
 
 st.set_page_config(
     page_title="Analyse et Prédiction du Churn Client",

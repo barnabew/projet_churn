8f3c9f0fd17d9180d1f4a8a97a8a53fcb19856ea
barnabew <barnabe.willenbucher@outlook.fr>
2025-10-20 14:13:42 +0200

    Add project introduction to textes.py
    Added introductory text explaining project objectives and model details.

---
+++ streamlit/textes.py
@@ -0,0 +1,6 @@
+intro= """
+L’objectif de ce projet est de créer un modèle permettant de prédire la résiliation de clients (churn) d’une entreprise de téléphonie. 
+Pour cela, nous utilisons un modèle de régression logistique, car il est le plus efficace dans ce cas (cf. fichier .ipynb). 
+Nous avons une première page qui présente les différents résultats de notre modèle (AUC, Recall, etc.),
+puis une interface interactive qui permet à l’utilisateur de connaître la probabilité de churn en fonction de différents paramètres saisis.
+"""
